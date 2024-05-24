806199b99b268b835dffc3246831700275e4357f
i4nc3l <i4nc3l@Celery>
2024-05-24 23:03:38 +0200

    non

--- traducteur.c
+++ traducteur.c
@@ -2,10 +2,11 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#include "lexer.h" #importe les fonctions du fichier lexer.c
+#include "lexer.h" //importe les fonctions du fichier lexer.c
 
 int main(){
 
   FILE* fichier = fopen("trad.ml", "r");;
-  
+  maillon* liste = lexeur(fichier);
+
 }
