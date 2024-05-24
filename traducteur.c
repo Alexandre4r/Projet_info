d3dc58dd9e5702265c6d982c271c149b8c3056af
i4nc3l <i4nc3l@Celery>
2024-05-24 22:55:22 +0200

    non

--- traducteur.c
+++ traducteur.c
@@ -5,8 +5,13 @@
 #include "lexer.h" #importe les fonctions du fichier lexer.c
 
 int main(){
+<<<<<<< HEAD
   
   //On ouvre le fichier C à traduire 
   FILE* fichier = fopen(fich.c, r);;
+=======
+
+  FILE* fichier = fopen("trad.ml", "r");;
+>>>>>>> ceb4825 (non)
   
 }
