45f96c3354fe570efc3279b089851991404737f8
i4nc3l <i4nc3l@Celery>
2024-05-24 23:13:29 +0200

    non

---
+++ trad.ml
@@ -0,0 +1,1 @@
+let is_stephanie_catin= True;;
--- traducteur.c
+++ traducteur.c
@@ -4,9 +4,14 @@
 #include <string.h>
 #include "lexer.h" //importe les fonctions du fichier lexer.c
 
+void imprim(maillon* m){
+  printf("%s",m->argument);
+}
+
 int main(){
 
-  FILE* fichier = fopen("trad.ml", "r");;
+  FILE* fichier = fopen("trad.ml", "r");
   maillon* liste = lexeur(fichier);
+  imprim(liste);
 
 }
