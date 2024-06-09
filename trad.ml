4d677ff7da69012e5e25b82782032d1a18099a29
Alexandre4r <alexandre.balcon21@gmail.com>
2024-06-09 21:12:44 +0200

    test

--- trad.ml
+++ trad.ml
@@ -1,27 +1,27 @@
 let f(i,j) = 
-(!i)+(!j);
+    (!i)+(!j);
 ;;
 let g(i,j) = 
-f(ref ((!i)), ref ((!j)))*(!i);
+    f(ref ((!i)), ref ((!j)))*(!i);
 ;;
 Printf.printf "UXU %d"  (g(ref (4), ref ( 5)));;
 let i = ref 0;; 
 Printf.printf "%dtest"  (!i);;
 while((!i) = 4) do 
-i := (!i)+1;
+    i := (!i)+1;
+    if((!i)=3) then begin 
+        for j = 0 to 10 do
+            Printf.printf "%dtest"  (!i);
+        done;
+    end
+done;;
 if((!i)=3) then begin 
-for j = 0 to 10 do
-Printf.printf "%dtest"  (!i);
- done;
- end
- done;;
-if((!i)=3) then begin 
-Printf.printf "%dtest"  (!i);
- end
+    Printf.printf "%dtest"  (!i);
+end
 else  begin 
-i := (!i)+1;
- end
+    i := (!i)+1;
+end
 let funct(i,j) = 
-(!i)+(!j);
+    (!i)+(!j)
 ;;
 (*erika miam*)(*Test****)
