8b787501643a59b0b67bebc2dcf3d4b362b10fe1
i4nc3l <celian.giaume@gmail.com>
2024-06-09 19:32:05 +0200

    fix le code de l'alsacien pr les fct

--- trad.ml
+++ trad.ml
@@ -1,7 +1,26 @@
+let f(i,j) = 
+(!i)+(!j);
+;;
+let g(i,j) = 
+f(ref ((!i)), ref ((!j)))*(!i);
+;;
+Printf.printf "UXU %d"  g(4  5);;
 let i = ref 0;; 
+Printf.printf "%dtest"  (!i);;
 while((!i) <> 4) do 
 i := (!i)+1;
 if((!i)==3) then begin 
+for j = 0 to 10 do
 Printf.printf "%dtest"  (!i);
-end
-done
+ done;
+ end
+ done;;
+if((!i)==3) then begin 
+Printf.printf "%dtest"  (!i);
+ end
+else  begin 
+i := (!i)+1;
+ end
+let funct(i,j) = 
+(!i)+(!j);
+;;
