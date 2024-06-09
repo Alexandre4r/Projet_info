2fe927944e99ef1281b9605f354ac3033f71bba4
Surezz-C <theohari71@gmail.com>
2024-06-09 17:55:55 +0200

    llh

--- fichier.c
+++ fichier.c
@@ -1,15 +1,20 @@
+int f(int i, int j){
+    return i+j;
+}
+int g(int i, int j){
+    return f(i,j)*i;
+}
+
 int main(){
-    int i = 5;
-    while(i>1){
-        if(i = 2){
-            printf("%d\n", i);
-        }else{
-            printf("UWU", i+1);
+    printf("UXU %d", funct(4, 5));
+    int i=0;
+    printf("%dtest", i);
+    while(i != 4){
+        i = i+1;
+        if(i==3){
+            printf("%dtest", i);
         }
     }
-
-
-}
     if(i==3){
         printf("%dtest", i);
     } else {
@@ -20,3 +25,4 @@ int funct(int i, int j){
     return i+j;
 }
 /*erika miam*/
+/*Test*/
